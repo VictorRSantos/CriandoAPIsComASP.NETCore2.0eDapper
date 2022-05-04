a990b2573daeb322f4e22042d361930d760f25ed
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-05-04 13:30:37 -0300

    OOP: modificadores

---
+++ BaltaStore/BaltaStore.Domain/StoreContext/Customer.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace BaltaStore.Domain.StoreContext
+{
+    public class Customer
+    {
+
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Salary { get; set; }
+
+        // Metodos
+        public void Register() { }
+
+        // Eventos
+        public void AoRegistrar() { }
+
+    }
+
+}
