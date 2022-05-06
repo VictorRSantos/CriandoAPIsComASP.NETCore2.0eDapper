a02c29932c07e6f0b2b512dc1d2d631ad3e9261c
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-05-06 12:08:03 -0300

    Value Objects

--- BaltaStore/BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore/BaltaStore.Tests/UnitTest1.cs
@@ -9,9 +9,9 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var c = new Customer("Andre", "Baltieri", "1234567811", "hello@balta.io", "1999999999", "Rua dos Developers, 1024");
+            //var c = new Customer("Andre", "Baltieri", "1234567811", "hello@balta.io", "1999999999", "Rua dos Developers, 1024");
 
-            var order = new Order(c);
+            //var order = new Order(c);
 
            
 
