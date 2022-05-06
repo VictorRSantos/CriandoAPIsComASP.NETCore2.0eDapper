78f9d8226d479e1b591dcac81022eab15d767d30
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-05-06 13:10:01 -0300

    Simulando o Pedido - Tests

--- BaltaStore/BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore/BaltaStore.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.ValueObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BaltaStore.Tests
@@ -9,11 +10,38 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            //var c = new Customer("Andre", "Baltieri", "1234567811", "hello@balta.io", "1999999999", "Rua dos Developers, 1024");
+            var name = new Name("Andre", "Baltieri");
+            var document = new Document("12345678911");
+            var email = new Email("hello@balta.io");          
+            var c = new Customer(name, document, email, "1999207414");
+            
+            
+            var mouse = new Product("Mouse", "Rato", "image.png", 59.90M, 10);
+            var teclado = new Product("Teclado", "Teclado", "image.png", 159.90M, 10);
+            var impressora = new Product("Impressora", "Impressora", "image.png", 359.90M, 10);
+            var cadeira = new Product("Cadeira", "Cadeira", "image.png", 559.90M, 10);
+       
+            var order = new Order(c);
+            order.AddItem(new OrderItem(mouse,5));
+            order.AddItem(new OrderItem(teclado,5));
+            order.AddItem(new OrderItem(cadeira,5));
+            order.AddItem(new OrderItem(impressora,5));
 
-            //var order = new Order(c);
+            // Realizei o pedido
+            order.Place();
 
-           
+
+            // Simula o pagamento
+            order.Pay();
+
+            // Simular o envio
+            order.Ship();
+
+            // Simular o cancelamento
+            order.Cancel();
+
+
+
 
 
         }
