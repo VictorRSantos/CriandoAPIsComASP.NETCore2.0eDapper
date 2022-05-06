c93a8588efeb1a5b517924ac8922a4ccf2b20b71
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-05-06 12:47:50 -0300

    Modelando o Pedido

--- BaltaStore/BaltaStore.Domain/StoreContext/Entities/Order.cs
+++ BaltaStore/BaltaStore.Domain/StoreContext/Entities/Order.cs
@@ -12,9 +12,8 @@ namespace BaltaStore.Domain.StoreContext.Entities
         private readonly IList<Delivery> _deliveries;
         public Order(Customer customer)
         {
-
             Customer = customer;
-            Number = Guid.NewGuid().ToString().Replace("-","").Substring(0,8).ToUpper();
+
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
             _items = new List<OrderItem>();
@@ -33,17 +32,67 @@ namespace BaltaStore.Domain.StoreContext.Entities
             _items.Add(item);
         }
 
-        public void AddDelivery(Delivery delivery)
-        {
-            _deliveries.Add(delivery);
-        }
+
 
         // To Place An Order
-        public void Place() { }
+        // Criar um pedido
+        public void Place()
+        {
+            // gera o número do pedido
+            Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 8).ToUpper();
+
+        }
+
+        // Pagar um pedido
+        public void Pay()
+        {
+           
+            Status = EOrderStatus.Paid;
+
+        }
+
+        // Enviar um pedido
+        public void Ship()
+        {
+            // A cada 5 produtos é uma entrega
+            var deliveries = new List<Delivery>();
+            var count = 1;
+
+
+            // Quebra as entregas
+            foreach (var item in _items)
+            {
+
+                if (count == 5)
+                {
+                    count = 1;
+                    deliveries.Add(new Delivery(DateTime.Now.AddDays(5)));
+                }
+
+
+            }
+
+            // Envia todas as entregas
+            deliveries.ForEach(x => x.Ship());
+
+            // Adiciona as entregas ao pedido
+            deliveries.ForEach(x => _deliveries.Add(x));
+                       
+
+        }
 
 
 
-     
+        // Cencelar um pedido
+        public void Cancel()
+        {
+            Status = EOrderStatus.Canceled;
+            _deliveries.ToList().ForEach(x => x.Cancel());        
+        
+        }
+
+
+
 
 
 
