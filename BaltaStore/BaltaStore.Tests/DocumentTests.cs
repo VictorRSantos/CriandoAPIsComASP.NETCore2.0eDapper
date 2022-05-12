454c65e4b9e1b3ace0056639048c9887b27be35d
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-05-12 13:49:01 -0300

    testando VO (Value Objects)

---
+++ BaltaStore/BaltaStore.Tests/DocumentTests.cs
@@ -0,0 +1,36 @@
+﻿using BaltaStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private Document _validDocument;
+        private Document _invalidDocument;
+        public DocumentTests()
+        {
+            
+            _validDocument = new Document("32052020829");
+            _invalidDocument = new Document("12345678910");
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid() 
+        {
+           
+            Assert.AreEqual(false, _invalidDocument.IsValid);
+            Assert.AreEqual(1, _invalidDocument.Notifications.Count);
+        }
+
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsValid()
+        {
+         
+            Assert.AreEqual(true, _validDocument.IsValid);
+            Assert.AreEqual(0, _validDocument.Notifications.Count);
+        }
+
+    }
+}
