1c13db0afaedf030907c7db1cbbb1a0f6017c90b
Victor Rodrigues dos Santos <victor.rsantos@outlook.com>
2022-05-18 11:48:34 -0300

    API: Criando endpoints para CRUD

---
+++ BaltaStore/BaltaStore.Api/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using BaltaStore.Domain.StoreContext.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace BaltaStore.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        [HttpGet]
+        [Route("customers")]
+        public List<Customer> Get()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customer/{id}")]
+        public Customer GetById(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customer/{id}/orders")]
+        public List<Order> GetOrders(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("customers")]
+        public Customer Post([FromBody] Customer customer)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        [Route("customers/{id}")]
+        public Customer Put([FromBody] Customer customer)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("customers/{id}")]
+        public string Delete()
+        {
+            return null;
+        }
+    }
+}
